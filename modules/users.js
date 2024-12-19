da58d48494f56a0e18539f5bbfdb9e75afec1158
Abhishek Kumar Gupta <abhishek.gupta1@synoriq.in>
2024-12-19 23:27:26 +0530

    User Api Response Handling

--- modules/users.js
+++ modules/users.js
@@ -3,19 +3,25 @@ const getData = require("../config/database");
 
 module.exports.getUser = async (req, res, next) => {
     const {email, password} = req.query;
-    console.log("Bodyyy>>>>>",req.query);
-        if(!email || !password){
-            console.log(">>........",req.body);
-            res.status(400);
-            res.message = "Please enter Email and Password.";
-            return next(res);
-        }
+    console.log("Params :",req.query);
+    if(!email || !password){
+        res.status(400);
+        res.message = "Please enter Email and Password.";
+        return next(res);
+    }
+
     try {
         const query = `select * from job_portal.users u where u.email = '${email}' and u.password = '${password}' and u.deleted = false`;
-        // const query = `select * from job_portal.users`;
         console.log("Query :",query);
         let user = await getData(query);
-        console.log("User Data>>>",user);
+        if(!user.length)
+            res.message = "No user data found."
+        console.log("User Data :",user);
+        res.json({
+            title: "Success",
+            message: res.message || "Data fetched successfully.",
+            data: user
+        });
     } catch (error) {
         console.log("Getting Error>>",error);
     }
